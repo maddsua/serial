30f843c81ae7f330bb6efb058231231057cc144b
maddsua <maddsuadev@pm.me>
2022-06-07 23:17:12 +0300

    init

---
+++ src-win32-cmd/SerialTerminal_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef SERIALTERMINAL_PRIVATE_H
+#define SERIALTERMINAL_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.4.240"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	4
+#define VER_BUILD	240
+#define COMPANY_NAME	"maddsua"
+#define FILE_VERSION	"1.0.4.240"
+#define FILE_DESCRIPTION	"Serial Terminal"
+#define INTERNAL_NAME	"SerialTerminal"
+#define LEGAL_COPYRIGHT	"https://gitlab.com/maddsua"
+#define LEGAL_TRADEMARKS	"2022 maddsua/dev"
+#define ORIGINAL_FILENAME	"serter"
+#define PRODUCT_NAME	"Serial Terminal"
+#define PRODUCT_VERSION	"1.0.4.240"
+
+#endif /*SERIALTERMINAL_PRIVATE_H*/
