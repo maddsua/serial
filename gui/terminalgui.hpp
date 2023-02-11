fd2b5c2807d066daeb9408e1752182c548e1aa07
maddsua <maddsuapro@gmail.com>
2023-02-12 00:58:02 +0200

    Update terminalgui.hpp

--- gui/terminalgui.hpp
+++ gui/terminalgui.hpp
@@ -21,7 +21,21 @@
 	};
 
 	struct uiData {
+		std::vector <std::string> speeds;
+		std::vector <std::string> ports;
 
+		std::string buffIn;
+		std::string buffOut;
+
+		int32_t commstat;
+
+		size_t sel_speed;
+		size_t sel_port;
+		size_t historyItem;
+
+		bool viewHistory;
+		bool useNewline;
+		bool isExtended;
 	};
 
 
